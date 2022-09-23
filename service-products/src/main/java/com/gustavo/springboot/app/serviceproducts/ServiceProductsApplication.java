764ae62cf4df23dd61f9147f294de5d8e3dc1d72
Gustavo Chiclla Castañeda <gustavohoper@gmail.com>
2022-09-23 17:43:41 -0500

    Add config eureka client service-products

--- service-products/src/main/java/com/gustavo/springboot/app/serviceproducts/ServiceProductsApplication.java
+++ service-products/src/main/java/com/gustavo/springboot/app/serviceproducts/ServiceProductsApplication.java
@@ -2,7 +2,9 @@ package com.gustavo.springboot.app.serviceproducts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient //-> Explicitly enable eureka client
 @SpringBootApplication
 public class ServiceProductsApplication {
 
