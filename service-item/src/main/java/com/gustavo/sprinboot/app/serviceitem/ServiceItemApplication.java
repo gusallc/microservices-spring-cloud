8df99b82197e70297574250ccf13ba2f8b77fbdd
Gustavo Chiclla Castañeda <gustavohoper@gmail.com>
2022-09-23 17:46:37 -0500

    Add config Eureka Client and delete ribbon service-item
    Ribbon is already included in eureka client

--- service-item/src/main/java/com/gustavo/sprinboot/app/serviceitem/ServiceItemApplication.java
+++ service-item/src/main/java/com/gustavo/sprinboot/app/serviceitem/ServiceItemApplication.java
@@ -2,10 +2,10 @@ package com.gustavo.sprinboot.app.serviceitem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "service-products") //-> @RibbonClients with 's' is for use with any services
+@EnableEurekaClient //-> Explicitly enable eureka client
 @EnableFeignClients
 @SpringBootApplication
 public class ServiceItemApplication {
