5cb6e606ee7ac587c90a68757655362c5c749df9
Gustavo Chiclla Castañeda <gustavohoper@gmail.com>
2023-01-13 22:23:14 -0500

    Added examples of CB and TL with fallbackmethod and CompletableFuture

--- service-item/src/main/java/com/gustavo/sprinboot/app/serviceitem/controllers/ItemController.java
+++ service-item/src/main/java/com/gustavo/sprinboot/app/serviceitem/controllers/ItemController.java
@@ -4,12 +4,14 @@ import com.gustavo.sprinboot.app.serviceitem.models.Item;
 import com.gustavo.sprinboot.app.serviceitem.models.Product;
 import com.gustavo.sprinboot.app.serviceitem.models.service.ItemService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/v1/items")
@@ -30,10 +32,25 @@ public class ItemController {
     }
 
     // This annotation only takes properties from the .yml or .properties configuration file.
-    @CircuitBreaker(name = "items",fallbackMethod = "alternativeMethod")
+    @CircuitBreaker(name = "items", fallbackMethod = "alternativeMethod")
     @GetMapping("/{id}")
     public Item details(@PathVariable Long id, @RequestParam Integer quantity) {
         return itemService.findById(id, quantity);
+    }
+
+    @TimeLimiter(name = "items", fallbackMethod = "alternativeMethodWithTimeLimiter")
+    @GetMapping("/timeLimiter/{id}")
+    public CompletableFuture<Item> detailsWithTimeLimiter(@PathVariable Long id, @RequestParam Integer quantity) {
+        return CompletableFuture.supplyAsync(() -> itemService.findById(id, quantity));
+    }
+
+    //the "fallbackMethod" of the circuit is the one that predominates when these 2 annotations are combined
+    // "alternativeMethodWithTimeLimiter" is used since the return type is "CompletableFuture"
+    @CircuitBreaker(name = "items", fallbackMethod = "alternativeMethodWithTimeLimiter")
+    @TimeLimiter(name = "items")
+    @GetMapping("/limiterAndCBreaker/{id}")
+    public CompletableFuture<Item> detailsWithTimeLimiterAndBreaker(@PathVariable Long id, @RequestParam Integer quantity) {
+        return CompletableFuture.supplyAsync(() -> itemService.findById(id, quantity));
     }
 
     //Here, we can consume another service via feign, restTemplate or other.
@@ -47,6 +64,18 @@ public class ItemController {
         item.setQuantity(quantity);
         item.setProduct(product);
         return item;
+    }
+
+    public CompletableFuture<Item> alternativeMethodWithTimeLimiter(Long id, Integer quantity, Throwable throwable) {
+        logger.info(throwable.getMessage());
+        Item item = new Item();
+        Product product = new Product();
+        product.setId(id);
+        product.setName("Attribute fallback");
+        product.setPrice(0.0);
+        item.setQuantity(quantity);
+        item.setProduct(product);
+        return CompletableFuture.supplyAsync(() -> item);
     }
 
 }
